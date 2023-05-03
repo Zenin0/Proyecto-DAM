6113ab63b60fd7e945b478b301928d5e18825698
Isaac Sanz <isaacsanzgomez102125@gmail.com>
2023-05-03 18:59:48 +0200

    Changed module-info

--- App/src/main/java/module-info.java
+++ App/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module app {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires transitive javafx.graphics;
+
 
     opens app to javafx.fxml;
     exports app;
